dad2a6647c6d5ff12d6fee980fe7b32ffc88e786
simeongug <simeon.guglielmetti@edu.ece.fr>
2023-11-17 11:34:48 -0500

    tp11

--- src/__tests__/fizzbuzz.test.ts
+++ src/__tests__/fizzbuzz.test.ts
@@ -2,32 +2,31 @@
 
 describe('FizzBuzz tdd test', () => {
 
-    beforeEach( () => {
-	})
+    beforeEach(() => {
 
-    it('write my first test', () => {
-        expect(1).toEqual(1)
+    })
+
+    it('should return the correct FizzBuzz array', () => {
+
+        function fizzBuzz(n: number): string[] {
+            const result: string[] = [];
+            for (let i = 1; i <= n; i++) {
+                if (i % 3 === 0 && i % 5 === 0) {
+                    result.push("FizzBuzz");
+                } else if (i % 3 === 0) {
+                    result.push("Fizz");
+                } else if (i % 5 === 0) {
+                    result.push("Buzz");
+                } else {
+                    result.push(i.toString());
+                }
+            }
+            return result;
+        }
+
+        // Test
+        expect(fizzBuzz(3)).toEqual(["1", "2", "Fizz"]);
+        expect(fizzBuzz(5)).toEqual(["1", "2", "Fizz", "4", "Buzz"]);
+        expect(fizzBuzz(15)).toEqual(["1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"]);
     });
 });
-
-
-// Given an integer n, return a string array answer (1-indexed) where:
-
-// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
-// answer[i] == "Fizz" if i is divisible by 3.
-// answer[i] == "Buzz" if i is divisible by 5.
-// answer[i] == i if non of the above conditions are true.
- 
-
-// Example 1:
-
-// Input: n = 3
-// Output: ["1","2","Fizz"]
-// Example 2:
-
-// Input: n = 5
-// Output: ["1","2","Fizz","4","Buzz"]
-// Example 3:
-
-// Input: n = 15
-// Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
